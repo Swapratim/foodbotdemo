263067570694f093c53f039e7ec124f9d29e631f
Swapratim <Swapratim@users.noreply.github.com>
2017-09-15 09:49:33 +0200

    Add files via upload

--- foodbot.py
+++ foodbot.py
@@ -21,12 +21,7 @@ from pymongo import MongoClient
 context = Flask(__name__)
 # Facbook Access Token (For FoodBot Facebook Page)
 ACCESS_TOKEN = "EAALflMNd24sBAB7pQyVOtc6nzzl8IwGQ3u7ZAqwc5DoqDhiwsxwPPIcqFwgFvZAazzLS415ZChO6ZBUpIi6RLeSA7ZBi2ZCFAhChDx1SQSDjMzmSHAu1PQ2P6tnEHM3Ko1cULn4XJt31ZBXs6IfXVC8xFXxZBf2La8fq4VdfgPuMTAZDZD"
-# Google Access Token
-Google_Acces_Token = "key=AIzaSyDNYsLn4JGIR4UaZMFTAgDB9gKN3rty2aM&cx=003066316917117435589%3Avcms6hy5lxs&q="
-# NewsAPI Access Token
-newspai_access_token = "505c1506aeb94ba69b72a4dbdce31996"
-# Weather Update API KeyError
-weather_update_key = "747d84ccfe063ba9"
+
 
 #************************************************************************************#
 #                                                                                    #
@@ -38,6 +33,8 @@ def webhook():
     reqContext = request.get_json(silent=True, force=True)
     if reqContext.get("result").get("action") == "input.welcome":
        return welcome()
+    elif reqContext.get("result").get("action") == "input.event":
+       return eventlist(reqContext)
     elif reqContext.get("result").get("action") == "Help":
        return help(reqContext)
     else:
@@ -78,15 +75,15 @@ def welcome():
           "template_type": "generic",
           "elements": [
             {
-              "title": "Hi " + first_name + ", Welcome to The South Indian Restaurant!",
-              "image_url": "http://s.dinnerbooking.eu/uploads/restaurant_pictures/530x250/1493285301_505-1348-the-south-indian-herning.jpg"
+              "title": "Hi " + first_name + ", Welcome to The Coco & Butter Restaurant!",
+              "image_url": "http://www.pdcdc.org/wp-content/uploads/2016/03/restaurant-939435_960_720.jpg"
             }
           ]
         }
       }
     },
     {
-      "text": "South Indian is dedicated to showcase the finest South Indian cuisine in Denmark."
+      "text": "Coco & Butter is dedicated to showcase the finest local cuisine in Denmark."
     },
     {
       "text": "I'm FoodBot. How can I help you?",
@@ -95,31 +92,31 @@ def welcome():
           "content_type": "text",
           "title": "Opening Hours",
           "payload": "openinghoursandlocation",
-          "image_url": "http://s3.amazonaws.com/libapps/accounts/26094/images/open-icon.jpg"
+          "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREBeHDCh_So0LEhyWapjjilpDFiRLXMaeuwUfc1rrxu3qShTCUqQ"
         },
         {
           "content_type": "text",
-          "title": "Discover Menu",
+          "title": "Menu",
           "payload": "Menu",
-          "image_url": "http://malaysiapca.com/wp-content/uploads/2016/09/pca-malaysia-chefs-table-food-menu-icon.jpg"
+          "image_url": "https://cdn1.iconfinder.com/data/icons/hotel-restaurant/512/16-512.png"
         },
         {
           "content_type": "text",
           "title": "Take Away",
           "payload": "Take Away",
-          "image_url": "http://discoverybayuk.com/wp-content/uploads/2015/09/takeaway-icon.gif"
+          "image_url": "https://d30y9cdsu7xlg0.cloudfront.net/png/66559-200.png"
         },
         {
           "content_type": "text",
           "title": "Event",
           "payload": "Event",
-          "image_url": "http://banburytown.com/wp-content/uploads/2015/08/icon_event.png"
+          "image_url": "http://icons.iconarchive.com/icons/icons8/windows-8/512/Time-Today-icon.png"
         },
         {
           "content_type": "text",
-          "title": "Help",
-          "payload": "Help",
-          "image_url": "https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-512.png"
+          "title": "Contact Us",
+          "payload": "contact",
+          "image_url": "http://www.logoeps.com/wp-content/uploads/2013/04/whatsapp-logo-symbol-vector.png"
         }
        ]
       }
@@ -142,7 +139,122 @@ def reply(user_id, msg):
     print (data)
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
- 
+
+#************************************************************************************#
+#                                                                                    #
+#   Below method is to get the Event Details as List Template                        #
+#                                                                                    #
+#************************************************************************************#
+newspaper_url = ''
+data = ''
+def eventlist(reqContext):
+    resolvedQuery = reqContext.get("result").get("resolvedQuery")
+    res = {
+            "speech": "Events",
+            "displayText": "Events",
+            "data" : {
+            "facebook" : [
+                 {
+                    "sender_action": "typing_on"
+                  },
+                 {
+                "attachment" : {
+                  "type" : "template",
+                    "payload" : {
+                     "template_type" : "list",
+                     "elements" : [ 
+                        {
+                            "title": "Århus - The Grand Opening",
+                            "image_url": "https://media-cdn.tripadvisor.com/media/photo-o/0d/b3/3a/55/the-south-indian-xpress.jpg",
+                            "default_action": {
+                               "type": "web_url",
+                               "url": "http://southindian.dk/calendar/the-south-indian-xpress-takeaway-arhus-the-grand-opening/",
+                                "webview_height_ratio": "tall",
+                                },
+                            "buttons": [
+                            {
+                               "title": "Read More",
+                               "type": "web_url",
+                               "url": "http://southindian.dk/calendar/the-south-indian-xpress-takeaway-arhus-the-grand-opening/",
+                               "webview_height_ratio": "tall",
+                            }
+                          ]
+                        },
+                        {
+                            "title": "Solrød: Frokost Tilbud",
+                            "image_url": "http://southindian.dk/wp-content/uploads/2017/03/Forkost-Tilbud2.png",
+                            "subtitle": "Enjoy the Food Festival",
+                            "default_action": 
+                                {
+                                    "type": "web_url",
+                                    "url": "http://southindian.dk/calendar/solrod-frokost-tilbud/",
+                                    "webview_height_ratio": "tall"
+                                },
+                                "buttons": [
+                                {
+                                     "title": "Read More",
+                                     "type": "web_url",
+                                     "url": "http://southindian.dk/calendar/solrod-frokost-tilbud/",
+                                     "webview_height_ratio": "tall"
+                                }
+                               ]
+                        },
+                        {
+                            "title": "Tamil Nytår",
+                            "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBd2nw84IbiTM1L3yy7yKTv6uCf7gJfv8cuIZrXog_G1YRsNweeQ",
+                            "subtitle": "4. November 2016 12:00",
+                            "default_action": 
+                               {
+                                   "type": "web_url",
+                                   "url": "http://southindian.dk/calendar/tamil-nytar-og-ny-frokostmenu/",
+                                   "webview_height_ratio": "tall"
+                                },
+                                "buttons": [
+                                {
+                                   "title": "Read More",
+                                   "type": "web_url",
+                                   "url": "http://southindian.dk/calendar/tamil-nytar-og-ny-frokostmenu/",
+                                   "webview_height_ratio": "tall"
+                                }
+                              ]
+                       },
+                       {
+                            "title": "Prøvespisning",
+                            "image_url": "http://southindian.dk/wp-content/uploads/2014/10/Up-close-clay-horse-of-Aiyanar-Temple-Chettinad-Tamil-Nadu-India.jpg",
+                            "subtitle": "1. November 2014 18:00, The Coco & Butter Godthåbsvej 12, 2000 Frederiksberg",
+                            "default_action": 
+                            {
+                                "type": "web_url",
+                                "url": "http://southindian.dk/calendar/provespisning/",
+                                "webview_height_ratio": "tall"
+                            },
+                            "buttons": [
+                            {
+                                "title": "Read More",
+                                "type": "web_url",
+                                "url": "http://southindian.dk/calendar/provespisning/",
+                                "webview_height_ratio": "tall"
+                            }
+                           ]
+                        }
+                        ],
+                        "buttons": [
+                         {
+                            "title": "View Site",
+                            "type": "web_url",
+                            "url": newspaper_url
+                        }
+                       ]  
+                     } 
+                   }
+                 }
+               ]
+             } 
+           };
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 #************************************************************************************#
 #                                                                                    #
