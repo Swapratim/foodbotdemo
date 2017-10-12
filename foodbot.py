9337b595db7e9c18611c2a58d3c9fac92df556cb
Swapratim <Swapratim@users.noreply.github.com>
2017-10-12 11:37:05 +0200

    Add files via upload

--- foodbot.py
+++ foodbot.py
@@ -37,6 +37,8 @@ def webhook():
        return eventlist(reqContext)
     elif reqContext.get("result").get("action") == "contact.us":
        return help(reqContext)
+    elif reqContext.get("result").get("action") == "menuitems":
+       return menuitems(reqContext)
     elif reqContext.get("result").get("action") == "howareyou":
        return howareyou(reqContext)
     else:
@@ -70,22 +72,26 @@ def welcome():
           "speech": speech,
           "displayText": speech,
            "data" : {
-              "facebook" : [{
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [
-            {
-              "title": "Hi " + first_name + ", Welcome to The Coco & Butter Restaurant!",
-              "image_url": "http://www.pdcdc.org/wp-content/uploads/2016/03/restaurant-939435_960_720.jpg"
+              "facebook" : [
+              {
+                    "sender_action": "typing_on"
+              },
+              {
+                   "attachment": {
+                   "type": "template",
+                   "payload": {
+                   "template_type": "generic",
+                   "elements": [
+                     {
+                       "title": "Hi " + first_name + ", Welcome to The Coco & Butter Restaurant!",
+                       "image_url": "http://www.pdcdc.org/wp-content/uploads/2016/03/restaurant-939435_960_720.jpg"
             }
           ]
         }
       }
     },
     {
-      "text": "Coco & Butter is dedicated to showcase the finest local cuisine in Denmark."
+      "text": "This is a Demo ChatBot. You can enjoy chatting and adopt it for your own restaurant."
     },
     {
       "text": "I'm FoodBot. How can I help you?",
@@ -144,11 +150,93 @@ def reply(user_id, msg):
 
 #************************************************************************************#
 #                                                                                    #
+#                       MENU ITEMS DISPLAY                                           #
+#                                                                                    #
+#************************************************************************************#
+def menuitems(reqContext):
+    resolvedQuery = reqContext.get("result").get("resolvedQuery")
+    res = {
+            "speech": "Events",
+            "displayText": "Events",
+            "data" : {
+            "facebook" : [
+                {
+                 "attachment" : {
+                   "type" : "template",
+                     "payload" : {
+                      "template_type" : "generic",
+                       "elements" : [ 
+                                 {
+                                   "title" : "Chef's Special",
+                                   "image_url" : "https://img.grouponcdn.com/deal/fqWyQpQoCk7Kzq51nszzcE/fuads_restaurant-2048x1229/v1/c700x420.jpg",
+                                   "subtitle" : " ",
+                                 },
+                                 {
+                                   "title" : "Chicken Mama Miya",
+                                   "image_url" : "https://www.bu.edu/today/files/2011/10/t_11-4183-INHOUSE-006.jpg",
+                                   "subtitle" : " ",
+                                 },
+                                 {
+                                   "title" : "Mexican Roll",
+                                   "image_url" : "http://img.taste.com.au/_k6sa3dZ/w720-h480-cfill-q80/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg",
+                                   "subtitle" : " ",
+                                 },
+                                 {
+                                   "title" : "Steak Special",
+                                   "image_url" : "http://www.quivertreeapartments.com/wp-content/uploads/Picture30-300x233.png",
+                                   "subtitle" : " ",
+                                 }
+                           ]
+                       } 
+                   }
+                }
+        {
+      "text": "I am better than ever :) \nHope you will enjoy close to heart delicious foods here. \n How can I help you?",
+      "quick_replies": [
+        {
+          "content_type": "text",
+          "title": "Opening Hours",
+          "payload": "openinghoursandlocation",
+          "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREBeHDCh_So0LEhyWapjjilpDFiRLXMaeuwUfc1rrxu3qShTCUqQ"
+        },
+        {
+          "content_type": "text",
+          "title": "Menu",
+          "payload": "Menu",
+          "image_url": "https://cdn1.iconfinder.com/data/icons/hotel-restaurant/512/16-512.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Take Away",
+          "payload": "Take Away",
+          "image_url": "https://d30y9cdsu7xlg0.cloudfront.net/png/66559-200.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Event",
+          "payload": "Event",
+          "image_url": "http://icons.iconarchive.com/icons/icons8/windows-8/512/Time-Today-icon.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Contact Us",
+          "payload": "contact",
+          "image_url": "https://cdn3.iconfinder.com/data/icons/communication-mass-media-news/512/phone_marketing-128.png"
+        }
+       ]
+      }]
+             } 
+           };
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+#************************************************************************************#
+#                                                                                    #
 #   Below method is to get the Event Details as List Template                        #
 #                                                                                    #
 #************************************************************************************#
-newspaper_url = ''
-data = ''
+
 def eventlist(reqContext):
     resolvedQuery = reqContext.get("result").get("resolvedQuery")
     res = {
@@ -251,18 +339,43 @@ def eventlist(reqContext):
                    }
                  },
                {
-                   "text": "Choose an item",
-                   "quick_replies": [
-               {
-                    "content_type": "text",
-                    "title": "Start Over",
-                    "payload": "hi"
-               }
-               ]
-               }
-               ]
-             } 
-           };
+      "text": "I'm FoodBot. How can I help you?",
+      "quick_replies": [
+        {
+          "content_type": "text",
+          "title": "Opening Hours",
+          "payload": "openinghoursandlocation",
+          "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREBeHDCh_So0LEhyWapjjilpDFiRLXMaeuwUfc1rrxu3qShTCUqQ"
+        },
+        {
+          "content_type": "text",
+          "title": "Menu",
+          "payload": "Menu",
+          "image_url": "https://cdn1.iconfinder.com/data/icons/hotel-restaurant/512/16-512.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Take Away",
+          "payload": "Take Away",
+          "image_url": "https://d30y9cdsu7xlg0.cloudfront.net/png/66559-200.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Event",
+          "payload": "Event",
+          "image_url": "http://icons.iconarchive.com/icons/icons8/windows-8/512/Time-Today-icon.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Contact Us",
+          "payload": "contact",
+          "image_url": "https://cdn3.iconfinder.com/data/icons/communication-mass-media-news/512/phone_marketing-128.png"
+        }
+       ]
+      }
+    ]
+  } 
+};
     res = json.dumps(res, indent=4)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
@@ -279,7 +392,7 @@ def howareyou(reqContext):
             "displayText": "Events",
             "data" : {
             "facebook" : [
-	{
+    {
       "text": "I am better than ever :) \nHope you will enjoy close to heart delicious foods here. \n How can I help you?",
       "quick_replies": [
         {
@@ -326,28 +439,94 @@ def howareyou(reqContext):
 #                                                                                    #
 #************************************************************************************#
 def help(resolvedQuery):
-    speech = "To reserve a table or order for food delivery, please contact us at: +45-7182-5584. Call our Customer Care now. :)"
+    print ("Within CONTACT US method")
+    speech = "Do you like FoodBot? \nRequest for a free Demo now. \nOur company is present in Denmark & Australia. \nGrow your business with AI Chatbot. "
     res = {
         "speech": speech,
         "displayText": speech,
         "data" : {
         "facebook" : [
+              {
+                 "text": speech
+                },
+                {
+                 "attachment" : {
+                   "type" : "template",
+                     "payload" : {
+                      "template_type" : "generic",
+                       "elements" : [ 
+                                 {
+                                   "title" : "Swapratim Roy",
+                                   "image_url" : "https://marvinchatbot.files.wordpress.com/2017/06/swapratim-roy-founder-owner-of-marvin-ai.jpg?w=700&h=&crop=1",
+                                   "subtitle" : "Founder & Owner of Marvin.ai \nAarhus, Denmark \nCall: +45-7182-5584",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "https://www.messenger.com/t/swapratim.roy",
+                                        "title": "Connect on Messenger"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://marvinai.live/about",
+                                        "title": "View Website"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Arnab Dasgupta",
+                                   "image_url" : "https://marvinchatbot.files.wordpress.com/2017/06/arnab-dasgupta-ceo-of-marvin-ai.jpg?w=700&h=&crop=1",
+                                   "subtitle" : "CEO of Marvin.ai \nMelbourne, Australia \nCall: +61-469-029-387",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "https://www.messenger.com/t/arnabdasgupta.uk",
+                                        "title": "Connect on Messenger"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://marvinai.live/about",
+                                        "title": "View Website"
+                                    }]
+                                 } 
+                           ]
+                       } 
+                   }
+                },
                {
-                "text": speech
-               },
-               {
-                   "text": "Choose an item",
-                   "quick_replies": [
-               {
-                    "content_type": "text",
-                    "title": "Start Over",
-                    "payload": "hi"
-               }
-               ]
-               }
-             ]
-           } 
-         };
+      "text": "I'm FoodBot. How can I help you?",
+      "quick_replies": [
+        {
+          "content_type": "text",
+          "title": "Opening Hours",
+          "payload": "openinghoursandlocation",
+          "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREBeHDCh_So0LEhyWapjjilpDFiRLXMaeuwUfc1rrxu3qShTCUqQ"
+        },
+        {
+          "content_type": "text",
+          "title": "Menu",
+          "payload": "Menu",
+          "image_url": "https://cdn1.iconfinder.com/data/icons/hotel-restaurant/512/16-512.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Take Away",
+          "payload": "Take Away",
+          "image_url": "https://d30y9cdsu7xlg0.cloudfront.net/png/66559-200.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Event",
+          "payload": "Event",
+          "image_url": "http://icons.iconarchive.com/icons/icons8/windows-8/512/Time-Today-icon.png"
+        },
+        {
+          "content_type": "text",
+          "title": "Contact Us",
+          "payload": "contact",
+          "image_url": "https://cdn3.iconfinder.com/data/icons/communication-mass-media-news/512/phone_marketing-128.png"
+        }
+       ]
+      }
+     ]
+    } 
+   };
     res = json.dumps(res, indent=4)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
